910e13f5e4fe501b7a31207ab5056f76b45d99ce
Sergiy Poddubka <poseal@gmail.com>
2021-06-18 23:32:16 +0300

    event handlers

--- src/App.js
+++ src/App.js
@@ -2,29 +2,24 @@
 import './App.css';
 
 function App() {
-  const posts = [{
-    id: 1,
-    title: 'Новость №1',
-    shotDescription: 'Краткое описание новости №1'
-  },
-  {
-    id: 2,
-    title: 'Новость №2',
-    shotDescription: 'Краткое описание новости №2'
-  },
-  {
-    id: 3,
-    title: 'Новость №3',
-    shotDescription: 'Краткое описание новости №3'
-  }];
+  const sendForm = () => {
+    alert("Форма отправлена");
+  };
+  const mouseEnter = () => {
+    console.log("Mouse enter");
+  };
+  const mouseLeave = () => {
+    console.log("Mouse leave");
+  };
   return (
     <div className="App">
-      {posts.map(post => (
-        <div key={post.id.toString()}>
-          <h2>{post.title}</h2>
-          <p>{post.shotDescription}</p>
-        </div>
-      ))}
+      <button
+        type="button"
+        onClick={sendForm}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}>
+        Отправить
+      </button>
     </div>
   );
 }
