0a6b3b6d20349397e71752137d6c7063c2ad5be9
Sergiy Poddubka <poseal@gmail.com>
2021-06-19 00:12:37 +0300

    render just once, however 2 useStates

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ function App() {
   // const setCounter = counterStateVariable[1];
   // or
   const [counter, setCounter] = useState(0);
+  const [myDate, setMyDate] = useState(new Date());
 
   const increaseCounter = () => {
     setCounter((prevCounter) => prevCounter + 1);
     setCounter((prevCounter) => prevCounter + 1);
+    setMyDate(new Date());
   };
+
+  console.log("render");
 
   return (
     <div className="App">
+      <div>{myDate.toISOString()}</div>
       {counter}
       <button type="button" onClick={increaseCounter}>Увеличить значение</button>
     </div>
